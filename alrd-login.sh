011cb46ed96fd5515497490117f7d8f0317bfdbd
Todd Partridge <toddrpartridge@gmail.com>
2014-09-30 09:48:54 -0400

    Added alrd-login.sh

---
+++ alrd-login.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+# login setups
+
+if [ ! -f /tmp/mandb.lock ]; then
+  echo "Updated man pages database, please wait..."
+  mandb --quiet && touch /tmp/mandb.lock
+fi
+
+clear 
+
+echo \
+"Arch Linux Rescue Disk
+  • alrd-setup - a live medium, basic configuration, setup script (recommended).
+  • alrd-doc   - documentation of the live medium and general install notes."
+
+eject
